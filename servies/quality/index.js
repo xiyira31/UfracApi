70b9ea809c4110c572cc161478b1371332254303
xiyira31 <xiyira31@gmail.com>
2020-08-11 19:10:58 +0800

    添加transaction

--- servies/quality/index.js
+++ servies/quality/index.js
@@ -1,17 +1,22 @@
 const Parser = require('expr-eval').Parser;
 const models  = require('../../db/models');
+const utils = require('../../utils/utils');
+const code = require('../../enums/RESPONSE_CODE');
+const {Sequelize} = require('sequelize');
 
 async function qualityServices (request) {
-  
-  return await exprPraser(request.well_info_id, request.function_id);
+  return await exprPraser(request.well_info_id, request.function_id, request.well_plan_id);
 }
 
-async function exprPraser(wellInfoId, quatityFuncId){
+async function exprPraser(wellInfoId, quatityFuncId, wellPlanId){
   const func = await models.proc_quatity_function.findOne({
     where: {
       id: quatityFuncId
     }
   });
+  if(func == null){
+    throw '没有对应方法:';
+  }
   const equation = func.equation;
   let parser = new Parser();
   let expr = parser.parse(equation);
@@ -23,16 +28,34 @@ async function exprPraser(wellInfoId, quatityFuncId){
       well: wellInfoId
     }
   });
-  let qualityResults = [];
-  varsVals.forEach(vals => {
-    let result = expr.evaluate(vals);
-    let qualityResult = {
-      md: vals.md,
-      value: result
-    };
-    qualityResults.push(qualityResult);
-  });
-  return qualityResults;
+  
+  try {
+    //console.log(sequelize);
+    const result = await models.sequelize.transaction(async (t) => {
+  
+      let well_quatity = await models.proc_well_quatity.create({
+        name:'',
+        wellplan: wellPlanId,
+        quatityfunction: quatityFuncId
+      }, { transaction: t });
+      let qualityResults = [];
+      varsVals.forEach(vals => {
+        let result = expr.evaluate(vals);
+        let qualityResult = {
+          md: vals.md,
+          value: result,
+          well_quatity: well_quatity.id
+        };
+        qualityResults.push(qualityResult);
+      });
+      await models.proc_well_quatity_detail.bulkCreate(qualityResults, { transaction: t });  
+    });
+  
+  } catch (error) {
+    throw error;
+  }
+
+  return utils.responseString(code.SUCCESS, "成功！");
 }
 
 module.exports = qualityServices;
