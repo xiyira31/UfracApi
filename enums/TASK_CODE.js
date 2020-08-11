5cf35c4e78aa30c8dccf626524a89a7ebf0a70d8
xiyira31 <xiyira31@gmail.com>
2020-08-11 19:09:40 +0800

    添加任务状态

---
+++ enums/TASK_CODE.js
@@ -0,0 +1,12 @@
+// 任务状态: 0=未计算，1=计算中，2=完成, 3=计算错误
+const NOT_CAL = 0;
+const CALING = 1;
+const FINISHED = 2;
+const ERROR = 3;
+
+module.exports = {
+  NOT_CAL,
+  CALING,
+  FINISHED,
+  ERROR
+}
