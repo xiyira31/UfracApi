0a7fee150b2de418ae7f01edc4b7b130c8f0ba3b
xiyira31 <xiyira31@gmail.com>
2020-08-14 16:15:17 +0800

    添加quatity的router

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var taskRouter = require('./routes/task');
+var quatityRouter = require('./routes/quatity');
 
 var app = express();
 
@@ -22,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/task', taskRouter);
+app.use('/quatity', quatityRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
