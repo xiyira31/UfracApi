77c50d1ed80bde666dabc0de4f3beaf823b8e633
xiyira31 <xiyira31@gmail.com>
2020-08-14 16:19:28 +0800

    暂存的task

--- routes/task.js
+++ routes/task.js
@@ -5,7 +5,11 @@ const RESPONSE_CODE = require('../enums/RESPONSE_CODE');
 const TASK_CODE = require('../enums/TASK_CODE');
 const utils = require('../utils/utils');
 let TaskServices = require('../servies/task');
-let QualityServices = require('../servies/quality')
+let QualityServices = require('../servies/quality');
+
+var Ajv = require('ajv');
+
+const qualitySchema = require('../schemas/quality.json');
 
 router.get('/:id',async function(req, res) {  
   const id = req.params.id;
@@ -38,9 +42,15 @@ router.get('/:id',async function(req, res) {
   let taskServices = new TaskServices(task);
   let request = JSON.parse(task.request_args);
   await taskServices.caling();
+  let ajv = new Ajv(); 
   try{
     switch(task.task_type){
       case 1:
+        var validate = ajv.compile(qualitySchema);
+        var valid = validate(request);
+        if (!valid) {
+          throw validate.errors;
+        }
         let qualityServices = new QualityServices(request);
         response = await qualityServices.exec();
         break;
@@ -52,6 +62,7 @@ router.get('/:id',async function(req, res) {
     console.log(error);
     response = utils.responseString(RESPONSE_CODE.ERROR, JSON.stringify(error));
     await taskServices.error(response);
+    res.json(response);
     return;
   }
   res.json(response);
