31faff5653a322818ee4491092d49264eca4a12f
xiyira31 <xiyira31@gmail.com>
2020-08-17 09:58:14 +0800

    解决跨域问题

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ var quatityRouter = require('./routes/quatity');
 
 var app = express();
 
+//设置跨域访问
+app.all('*', (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By",' 3.2.1')
+  res.header("Content-Type", "application/json;charset=utf-8");
+  next();
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
